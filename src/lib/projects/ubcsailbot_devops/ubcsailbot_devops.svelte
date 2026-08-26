7489126cfc78228f69de618d8a7e0a98f0a377c9
digcheese <jeffxue269@gmail.com>
2026-08-26 05:14:01 +0000

    start writing for sailbot project page

--- src/lib/projects/ubcsailbot_devops/ubcsailbot_devops.svelte
+++ src/lib/projects/ubcsailbot_devops/ubcsailbot_devops.svelte
@@ -1,6 +1,7 @@
 <script module lang="ts">
   import type { ProjectMetadata } from "$lib/components/ProjectData";
-  // import icon from "$lib/projects/ubcsailbot_devops/icon.png";
+  import icon from "$lib/projects/ubcsailbot_devops/icon.jpg";
+  import polaris_gui from "$lib/projects/ubcsailbot_devops/polaris_gui.png";
 
   export const metadata: ProjectMetadata = {
     slug: "ubcsailbot_devops",
@@ -8,10 +9,20 @@
     id: 98,
     description:
       "A summary of my contributions to the UBC Sailbot engineering design team as the Developer Operations Team Lead.",
-    // icon: icon,
-    // icon_alt: "",
+    icon: icon,
+    icon_alt:
+      "POLARIS on the water for its official launch. Photo not taken by me.",
   };
 </script>
 
-something about polaris gui. something about the release-image. something about
-the devcontainer
+<p>
+  I joined UBC Sailbot in September 2025 as a member of the Develeoper
+  Operations subteam and started working on optimizing development environments,
+  deployment workflows, and tools used during on-water testing.
+</p>
+
+<img src={polaris_gui} alt="Screenshot of the POLARIS GUI." />
+
+The POLARIS GUI began as side project vibecoded by the electrical team for
+testing. As the something about polaris gui. something about the release-image.
+something about the devcontainer
